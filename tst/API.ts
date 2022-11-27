15e3ef65a00cf3f86708fe77b2bce010f10335c7
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2022-11-27 14:35:58 +0100

    Add PNG placeholder

--- tst/API.ts
+++ tst/API.ts
@@ -3,7 +3,7 @@ import test from '@typescriptlibs/tst';
 
 const API = new Mastodon.API({
     access_token: '0', // test server does not validate
-    api_url: 'http://localhost:8000/'
+    api_url: 'http://localhost:8000/v1/'
 });
 
 test('Test API.getAccount fail', async (assert: test.Assert) => {
