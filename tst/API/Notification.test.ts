c23e352fd837cfca1f28f18394c1fd54ffb01770
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-01-24 21:10:01 +0100

    Add Notification.test.ts

--- tst/API/Notification.test.ts
+++ tst/API/Notification.test.ts
@@ -17,7 +17,7 @@
  * */
 
 import * as Mastodon from 'tsl-mastodon-api';
-import TestUtilities from './TestUtilities.js';
+import TestUtilities from '../TestUtilities.js';
 import test from '@typescriptlibs/tst';
 
 /* *
@@ -26,12 +26,17 @@ import test from '@typescriptlibs/tst';
  *
  * */
 
-const API = new Mastodon.API( {
+const APID = new Mastodon.API( {
     access_token: '0', // test server does not validate
     api_url: 'http://127.0.0.1:8000/v1-delete/'
 } );
 
-TestUtilities.forceGetFetch( API );
+const APIGM = new Mastodon.API( {
+    access_token: '0', // test server does not validate
+    api_url: 'http://127.0.0.1:8000/v1-get-multiple/'
+} );
+
+TestUtilities.forceGetFetch( APID );
 
 /* *
  *
@@ -39,13 +44,13 @@ TestUtilities.forceGetFetch( API );
  *
  * */
 
-test( 'Test API.deleteList', async ( assert: test.Assert ) => {
+test( 'Test API.getNotifications', async ( assert: test.Assert ) => {
     try {
-        const { json: list } = await API.deleteList( 'ID-8' );
-        assert.strictEqual(
-            list.id,
-            'ID-8',
-            'List ID should contain mockup value.'
+        const { json: notifications } = await APIGM.getNotifications();
+        assert.equal(
+            notifications[0].id,
+            'ID-10',
+            'Notification ID should contain mockup value.'
         );
     }
     catch ( result: any ) {
@@ -57,13 +62,13 @@ test( 'Test API.deleteList', async ( assert: test.Assert ) => {
     }
 } );
 
-test( 'Test API.deleteStatus', async ( assert: test.Assert ) => {
+test( 'Test API.deleteNotification', async ( assert: test.Assert ) => {
     try {
-        const { json: status } = await API.deleteStatus( 'ID-7' );
-        assert.strictEqual(
-            status.id,
-            'ID-7',
-            'Status ID should contain mockup value.'
+        const { json: body } = await APID.deleteNotification( 'ID-11' );
+        assert.deepEqual(
+            body,
+            {},
+            'Response should contain a {} body.'
         );
     }
     catch ( result: any ) {
@@ -73,4 +78,4 @@ test( 'Test API.deleteStatus', async ( assert: test.Assert ) => {
             'Request should not fail.'
         );
     }
-} );
+} )
