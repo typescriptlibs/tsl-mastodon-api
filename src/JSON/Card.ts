5818f603bb5b06a941c1fa778bbefbd91a3658cc
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-02-03 21:44:48 +0100

    Add doc comments to JSON.Card

--- src/JSON/Card.ts
+++ src/JSON/Card.ts
@@ -16,46 +16,185 @@
  *
  * */
 
-interface BaseCard {
+/**
+ * Represents a rich preview card that is generated using OpenGraph tags from a
+ * URL.
+ */
+interface BasePreviewCard {
+    /**
+     * Description of preview.
+     * @since 1.0.0
+     */
     description: string;
+    /**
+     * Title of linked resource.
+     * @since 1.0.0
+     */
     title: string;
+    /**
+     * The type of the preview card.
+     * @since 1.3.0
+     */
     type: string;
+    /**
+     * Location of linked resource.
+     * @since 1.0.0
+     */
     url: string;
 }
 
+/**
+ * Represents a rich preview card that is generated using OpenGraph tags from an
+ * URL.
+ */
 export type Card = (
-    LinkCard
+    | LinkCard
     | PhotoCard
+    | RichPreviewCard
     | VideoCard
 );
 
-export interface LinkCard extends BaseCard {
+/**
+ * Represents an iframe preview card that is generated using OpenGraph tags from
+ * an URL. Iframe is not currently accepted, so won’t show up in practice.
+ */
+export interface RichPreviewCard extends BasePreviewCard {
+    /**
+     * The type of the preview card.
+     * @since 1.3.0
+     */
+    type: 'rich';
+}
+
+/**
+ * Represents a link preview card that is generated using OpenGraph tags from an
+ * URL.
+ */
+export interface LinkCard extends BasePreviewCard {
+    /**
+     * The type of the preview card.
+     * @since 1.3.0
+     */
     type: 'link';
 }
 
-export interface PhotoCard extends BaseCard {
+/**
+ * Represents a photo preview card that is generated using OpenGraph tags from
+ * an URL.
+ */
+export interface PhotoCard extends BasePreviewCard {
+    /**
+     * The author of the original resource.
+     * @since 1.3.0
+     */
     author_name: string;
+    /**
+     * A link to the author of the original resource.
+     * @since 1.3.0
+     */
     author_url: string;
+    /**
+     * A hash computed by
+     * [the BlurHash algorithm](https://github.com/woltapp/blurhash), for
+     * generating colorful preview thumbnails when media has not been downloaded
+     * yet.
+     * @since 3.2.0
+     */
     blurhash: string;
+    /**
+     * Used for photo embeds, instead of custom `html`.
+     * @since 2.1.0
+     */
     embed_url: string;
+    /**
+     * Height of preview, in pixels.
+     * @since 1.3.0
+     */
     height: number;
-    image: string;
+    /**
+     * Preview thumbnail.
+     * @since 1.0.0
+     */
+    image?: string;
+    /**
+     * The provider of the original resource.
+     * @since 1.3.0
+     */
     provider_name: string;
+    /**
+     * A link to the provider of the original resource.
+     * @since 1.3.0
+     */
     provider_url: string;
+    /**
+     * The type of the preview card.
+     * @since 1.3.0
+     */
     type: 'photo';
+    /**
+     * Width of preview, in pixels.
+     * @since 1.3.0
+     */
     width: number;
 }
 
-export interface VideoCard extends BaseCard {
+/**
+ * Represents a video preview card that is generated using OpenGraph tags from
+ * an URL.
+ */
+export interface VideoCard extends BasePreviewCard {
+    /**
+     * The author of the original resource.
+     * @since 1.3.0
+     */
     author_name: string;
+    /**
+     * A link to the author of the original resource.
+     * @since 1.3.0
+     */
     author_url: string;
-    blurhash: string;
+    /**
+     * A hash computed by
+     * [the BlurHash algorithm](https://github.com/woltapp/blurhash), for
+     * generating colorful preview thumbnails when media has not been downloaded
+     * yet.
+     * @since 3.2.0
+     */
+    blurhash?: string;
+    /**
+     * Height of preview, in pixels.
+     * @since 1.3.0
+     */
     height: number;
+    /**
+     * HTML to be used for generating the preview card.
+     * @since 1.3.0
+     */
     html: string;
-    image: string;
+    /**
+     * Preview thumbnail.
+     * @since 1.0.0
+     */
+    image?: string;
+    /**
+     * The provider of the original resource.
+     * @since 1.3.0
+     */
     provider_name: string;
+    /**
+     * A link to the provider of the original resource.
+     * @since 1.3.0
+     */
     provider_url: string;
+    /**
+     * The type of the preview card.
+     * @since 1.3.0
+     */
     type: 'video';
+    /**
+     * Width of preview, in pixels.
+     * @since 1.3.0
+     */
     width: number;
 }
 
