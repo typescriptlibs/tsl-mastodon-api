ac98ec9df7f8b07ac46bf72470b207e3d79b9605
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-02-03 21:23:01 +0100

    Add doc comments to JSON.Applications

--- src/JSON/Application.ts
+++ src/JSON/Application.ts
@@ -16,8 +16,36 @@
  *
  * */
 
+/**
+ * Represents an application that interfaces with the REST API to access
+ * accounts or post statuses.
+ */
 export interface Application {
+    /**
+     * Client ID key, to be used for obtaining OAuth tokens.
+     * @since 0.9.9
+     */
+    client_id?: string;
+    /**
+     * Client secret key, to be used for obtaining OAuth tokens.
+     * @since 0.9.9
+     */
+    client_secret?: string;
+    /**
+     * The name of your application.
+     * @since 0.9.9
+     */
     name: string;
+    /**
+     * Used for Push Streaming API. Returned with POST /api/v1/apps. Equivalent
+     * to WebPushSubscription#server_key.
+     * @since 2.8.0
+     */
+    vapid_key?: string;
+    /**
+     * The website associated with your application.
+     * @since 0.9.9
+     */
     website?: ( string | null );
 }
 
