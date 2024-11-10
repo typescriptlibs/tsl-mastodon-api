2dd375fbd291f292afd40683febcc81d42c16642
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2024-11-10 14:59:12 +0100

    Add admin report test

---
+++ tst/API/AdminReport.test.ts
@@ -0,0 +1,41 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+  TypeScript Library for the Mastodon API
+
+  Copyright (c) TypeScriptLibs and Contributors
+
+  Licensed under the MIT License.
+  You may not use this file except in compliance with the License.
+  You can get a copy of the License at https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+import Setup from '../Setup.js';
+import test from '@typescriptlibs/tst';
+
+/* *
+ *
+ *  Tests
+ *
+ * */
+
+test( 'Test API.getAdminReport', async ( assert: test.Assert ) => {
+    try {
+        const { json: adminReport } = await Setup.v1Get.getAdminReport( 'ID-2' );
+        assert.strictEqual(
+            adminReport.id,
+            'ID-2',
+            'AdminReport ID should contain mockup value.'
+        );
+    }
+    catch ( result: any ) {
+        console.debug( result );
+        assert.fail( 'Request should not fail.' );
+    }
+} );
